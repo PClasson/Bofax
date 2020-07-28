fae0a8d4c279ce7625b033c55fdbd40d0ea163e4
PClasson <classon.philip@gmail.com>
2020-07-28 12:16:06 +0200

    Clean up

--- Bofax/Program.cs
+++ Bofax/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazor.IndexedDB.Framework;
+using MatBlazor;
 
 namespace Bofax
 {
@@ -18,6 +20,17 @@ namespace Bofax
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<IIndexedDbFactory, IndexedDbFactory>();
+            builder.Services.AddMatToaster(config =>
+                {
+                    config.Position = MatToastPosition.BottomRight;
+                    config.PreventDuplicates = true;
+                    config.NewestOnTop = true;
+                    config.ShowCloseButton = true;
+                    config.MaximumOpacity = 100;
+                    config.VisibleStateDuration = 3000;
+                    config.ShowProgressBar = false;
+                });
 
             await builder.Build().RunAsync();
         }
